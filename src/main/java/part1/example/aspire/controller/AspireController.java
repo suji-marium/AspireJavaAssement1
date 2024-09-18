f223c466a6974fc048a530d7089ad7900824cb1c
Stevin Reny Emerson(UST <117846920+StevinReny@users.noreply.github.com>
2024-09-18 17:25:12 +0530

    Added GetMapping for AspireController

--- src/main/java/part1/example/aspire/controller/AspireController.java
+++ src/main/java/part1/example/aspire/controller/AspireController.java
@@ -2,12 +2,15 @@ package part1.example.aspire.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import part1.example.aspire.model.Employee;
+import part1.example.aspire.model.EmployeeResponseGet;
 import part1.example.aspire.model.EmployeeResponseUpdate;
 import part1.example.aspire.service.AspireService;
 
@@ -18,8 +21,14 @@ public class AspireController {
     private AspireService aspireService;
 
     @PostMapping("/addEmployee")
-    public ResponseEntity<EmployeeResponseUpdate> addEmployee(@RequestBody Employee employee ){
+    public ResponseEntity<EmployeeResponseUpdate> addEmployee(@RequestBody Employee employee) {
         return aspireService.addEmployee(employee);
+    }
+
+    @GetMapping("/employee-name-start")
+    public ResponseEntity<EmployeeResponseGet> getEmployee(@RequestParam(value = "startletter") String startletter) {
+        System.out.println(startletter);
+        return aspireService.getEmployee(startletter);
     }
 
 }
--- src/main/java/part1/example/aspire/service/AspireService.java
+++ src/main/java/part1/example/aspire/service/AspireService.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import part1.example.aspire.model.Employee;
+import part1.example.aspire.model.EmployeeResponseGet;
 import part1.example.aspire.model.EmployeeResponseUpdate;
 import part1.example.aspire.repository.EmployeeRepository;
 
@@ -16,6 +17,10 @@ public class AspireService {
         employeeRepository.save(employee);
         EmployeeResponseUpdate employeeResponseUpdate=new EmployeeResponseUpdate("Employee added successfully");
         return ResponseEntity.ok(employeeResponseUpdate);
+    }
+    public ResponseEntity<EmployeeResponseGet> getEmployee(String startletter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEmployee'");
     }
 
     
